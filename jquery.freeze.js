15ac100cfd266db85d6a0da3fb55376b37385261
Victor Lin <vip.ljf@gmail.com>
2013-03-31 17:53:30 +0800

    do some clean up

--- jquery.freeze.js
+++ jquery.freeze.js
@@ -2,12 +2,13 @@
  * table header freezer v 0.01
  * author: vlin
  * */
+/*global document, window, jQuery, console*/
 (function($) {
     'use strict';
     $.fn.freeze = function(options) {
         var opts = $.extend({
-                marginTop : 0
-            }, options || {}),
+            marginTop: 0
+        }, options || {}),
             freeze = [];
 
         this.each(function() {
@@ -18,28 +19,28 @@
                 return false;
             }
             var cloneNode = parent.clone(!0),
-                offset = parent.offset();
+                offset = parent.offset(),
+                allChildren = thead.find('*');
 
             cloneNode.css({
-                left : offset.left,
-                top : opts.marginTop,
-                width : parent.width(),
-                display : 'none',
-                position : 'fixed'
+                left: offset.left,
+                top: opts.marginTop,
+                width: parent.width(),
+                display: 'none',
+                position: 'fixed'
             })
-            .removeAttr('id')
+                .removeAttr('id')
                 .children(':not(thead)').remove();
 
-            var allChildren = thead.find('*');
             $.each(cloneNode.find('thead *'), function(i) {
                 $(this).width(allChildren.eq(i).width());
             });
 
             freeze.push({
-                node : cloneNode,
-                offset : offset,
-                height : thead.height(),
-                parentHeight : parent.height()
+                node: cloneNode,
+                offset: offset,
+                height: thead.height(),
+                parentHeight: parent.height()
             });
 
             $(document.body).append(cloneNode);
@@ -48,17 +49,24 @@
         $(window).resize(function() {
             $(this).trigger('scroll');
         }).scroll(function() {
-            var sTop = $(window).scrollTop(),
-                sLeft = $(window).scrollLeft();
+            var sTop = $(window).scrollTop();
 
             $.each(freeze, function(i, f) {
                 var bottom = f.offset.top + f.parentHeight,
                     freezeLimit = bottom - f.height;
 
                 if (sTop > f.offset.top && sTop <= freezeLimit) {
-                    f.node.css({position : 'fixed', top : opts.marginTop, display : 'table'});
+                    f.node.css({
+                        position: 'fixed',
+                        top: opts.marginTop,
+                        display: 'table'
+                    });
                 } else if ((sTop > freezeLimit && sTop <= bottom)) {
-                    f.node.css({position : 'absolute', top : freezeLimit, display : 'table'});
+                    f.node.css({
+                        position: 'absolute',
+                        top: freezeLimit,
+                        display: 'table'
+                    });
                 } else {
                     f.node.hide();
                 }
